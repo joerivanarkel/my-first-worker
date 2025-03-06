f69415081bf93a30d51ea24570e6316e52033c0f
joerivanarkel <Joeri@vanarkel.net>
2025-03-06 20:00:32 +0100

    Add weather endpoint to fetch weather forecast

--- src/index.js
+++ src/index.js
@@ -7,9 +7,18 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { getWeatherForecast }  from './service/weatherForecastService.js';
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+        if (url.pathname === '/weather') {
+            const weatherForecast = getWeatherForecast();
+            return new Response(JSON.stringify(weatherForecast), {
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+
+        return new Response('Hello World!');
+    },
 };
---
+++ src/service/weatherForecastService.js
@@ -0,0 +1,9 @@
+export function getWeatherForecast() {
+    return {
+        location: 'San Francisco',
+        forecast: 'Sunny',
+        temperature: '20°C',
+        humidity: '60%',
+        wind: '5 km/h',
+    };
+}
